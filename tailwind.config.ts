85f706ed145d2d83d0f2b1d213f7a8b8f7acf4ec
bibekoli <git@bibekoli.com>
2024-12-28 20:34:18 +0545

    add custom color variables to global styles and extend Tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "primary": "var(--primary)",
+        "secondary": "var(--secondary)",
+        "tertiary": "var(--tertiary)",
+        "quaternary": "var(--quaternary)"
       },
     },
   },
